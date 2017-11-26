07cc0dfee2ebf2a1daaf81ee283c73d31845fd96
Alexander Kjäll <alexander.kjall@di.no>
2017-11-26 10:58:50 +0100

    solution for highsource

---
+++ highsource/getFlag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Look at source, get password; I4m4M4st3rC0d3rH4x0rsB3w43
+
+#enter password be redirected to this:
+
+curl 'http://highsource.tuctf.com/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flagdir/flag' -H 'Host: highsource.tuctf.com' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: */*' -H 'Accept-Language: sv-SE,en-US;q=0.7,en;q=0.3' --compressed -H 'Referer: http://highsource.tuctf.com/' -H 'origin: http://highsource.tuctf.com' -H 'Connection: keep-alive'
+
+#TUCTF{H1gh_S0urc3_3qu4ls_L0ng_F4ll}
